a7b7ae09ecd2b2fca0f8bdc0aa16c5c94898b0e7
Will Patton <willapatton@icloud.com>
2026-09-12 12:03:29 -0400

    Fix ALLOWED_HOSTS

--- moviesstore/settings.py
+++ moviesstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['willap.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
